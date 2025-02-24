946b5c1273d6edcdc5455123ee281fd374f8e29f
Rajnish1947 <kumarrajnish28443@gmail.com>
2025-02-24 18:10:32 +0530

    Update Transaction.jsx

--- frontend/src/pages/Transaction.jsx
+++ frontend/src/pages/Transaction.jsx
@@ -12,7 +12,7 @@ const Transaction = () => {
   useEffect(() => {
     const fetchDonors = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/donate/donors");
+        const response = await fetch("https://assignment-z3c4.onrender.com/api/donate/donors");
         const data = await response.json();
         setDonors(data);
       } catch (error) {
